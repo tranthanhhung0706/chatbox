2f5e6644dda1254b5cb47a3db6b592db2cc84cbd
tranthanhhung0706 <67146014+tranthanhhung0706@users.noreply.github.com>
2023-08-21 13:40:30 +0700

    ko

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = require('axios');
+//const axios = require('axios');
 import request from "request";
 import homepageService from "../services/homepageService";
 
